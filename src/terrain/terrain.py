352758b50755cd4f758fcf05af1a7dcf95babf77
Woojin Song <wsong31@my.bcit.ca>
2025-07-04 14:09:31 -0700

    Add pseudocode for Terrain abstract class

---
+++ src/terrain/terrain.py
@@ -0,0 +1,19 @@
+"""
+Contains abstract class for terrains.
+"""
+
+from abc import ABC, abstractmethod
+
+class Terrain(ABC):
+    pass
+# Max unit count per terrain type
+    # Absolute max
+    # Current max
+
+# Grid structure of the terrain
+
+# Buffs for units in terrain
+
+# Which units does it buff
+
+# Boolean passable or not
